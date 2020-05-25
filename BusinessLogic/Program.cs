f5902cd7fe5c1b471cd9064267206f89ae3be7b5
unknown <fakeisback4@gmail.com>
2020-05-25 16:54:47 +0300

    Started creating UI. Displaying all activities that anti cafe has

--- BusinessLogic/Program.cs
+++ BusinessLogic/Program.cs
@@ -12,7 +12,21 @@ namespace BusinessLogic
     {
         static void Main(string[] args)
         {
-           
+            UnitOfWork unitOfWork = new UnitOfWork();
+
+            Console.WriteLine("WELCOME TO THE ANTICAFE");
+            Console.WriteLine("Our Activities. Choose from list: ");
+            var activities = unitOfWork.activitiesRepository.GetAll();
+            for (int i = 0; i < activities.Count; i++)
+            {
+                Console.WriteLine(i + ") " + activities[i].Name + ". " + activities[i].Description);
+                if (i == activities.Count - 1)
+                    Console.WriteLine(i + 1 + ") Special. Create special event");
+            }
+
+            Console.WriteLine("Enter the number of activity you are interested in: ");
+            int activity_number = Convert.ToInt32(Console.ReadLine());
+            Console.Read();
         }
 
     }
