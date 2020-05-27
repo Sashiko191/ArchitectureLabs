ee159c1f0eb5c8d3ab3aa69d481693da70b3ed9f
unknown <fakeisback4@gmail.com>
2020-05-27 14:27:22 +0300

    Created new project for UI level. Removed interfaces and realizations of repository. Created IGenericRepository interface. Realized IGenericInterface. Deleted old unitOfWork. Unit of work moved to business logic layer

---
+++ BusinessLogic/UnitOfWorkRealization/UnitOfWork.cs
@@ -0,0 +1,60 @@
+﻿using DB_Layer.Interfaces;
+using DB_Layer.Models;
+using DB_Layer.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.UnitOfWorkRealization
+{
+    public class UnitOfWork
+    {
+        private static UnitOfWork instance;
+        public AntiCafeDb db;
+
+        public IGenericRepository<Order> ordersRepository;
+        public IGenericRepository<Room> roomsRepository;
+        public IGenericRepository<Equipment> equipmentRepository;
+        public IGenericRepository<Activity> activitiesRepository;
+
+        private UnitOfWork()
+        {
+            db = new AntiCafeDb();
+            ordersRepository = new GenericRepository<Order>(db);
+            roomsRepository = new GenericRepository<Room>(db);
+            equipmentRepository = new GenericRepository<Equipment>(db);
+            activitiesRepository = new GenericRepository<Activity>(db);
+        }
+
+        public void SaveDB()
+        {
+            db.SaveChanges();
+        }
+        public static UnitOfWork GetUnitOfWork()
+        {
+            if (instance == null)
+                instance = new UnitOfWork();
+            return instance;
+        }
+
+        private bool disposed = false;
+        public virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    db.Dispose();
+                }
+                this.disposed = true;
+            }
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ DB_Layer/Repositories/GenericRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DB_Layer.Interfaces;
+using DB_Layer.Models;
+
+namespace DB_Layer.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private AntiCafeDb db;
+        private DbSet<T> _dbSet;
+
+        public GenericRepository(AntiCafeDb context)
+        {
+            db = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public void Create(T item)
+        {
+            _dbSet.Add(item);
+            db.SaveChanges();
+        }
+
+        public T FindById(int id)
+        {
+            return _dbSet.Find(id);
+        }
+
+        public IEnumerable<T> Get()
+        {
+            return _dbSet.ToList();
+        }
+
+        public IEnumerable<T> Get(Func<T, bool> predicate)
+        {
+            return _dbSet.Where(predicate).ToList();
+        }
+
+        public void Remove(T item)
+        {
+            _dbSet.Remove(item);
+            db.SaveChanges();
+        }
+
+        public void Update(T item)
+        {
+            db.Entry(item).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}
