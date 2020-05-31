a5205813d6497a4127e2b8d3baf9fef838ae6e7c
unknown <fakeisback4@gmail.com>
2020-05-31 23:14:49 +0300

    Created DI Resolver for WebApi Level

---
+++ WebApi/Utils/WebApiDIResolver.cs
@@ -0,0 +1,42 @@
+﻿using BusinessLogic.BL_Repositories;
+using BusinessLogic.Interfaces;
+using BusinessLogic.Models;
+using BusinessLogic.Services;
+using Ninject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApi.Interfaces;
+using WebApi.Services;
+
+namespace WebApi.Utils
+{
+    public class WebApiDIResolver
+    {
+        private static IKernel NinjectResolver;
+        static WebApiDIResolver()
+        {
+            NinjectResolver = new StandardKernel();
+            NinjectResolver.Bind<IBusinessLogicRepository<ActivityDTO>>().To<ActivitiesBLRepository>();
+            NinjectResolver.Bind<IBusinessLogicRepository<OrderDTO>>().To<OrdersBLRepository>();
+            NinjectResolver.Bind<IBusinessLogicRepository<EquipmentDTO>>().To<EquipmentBLRepository>();
+            NinjectResolver.Bind<IBusinessLogicRepository<RoomDTO>>().To<RoomsBLRepository>();
+            NinjectResolver.Bind<IViewModelMappingConfigsGenerator>().To<ViewModelMappingConfigsGenerator>();
+           
+            NinjectResolver.Bind<IActivityService>().To<ActivityService>();
+            NinjectResolver.Bind<IOrderService>().To<OrderService>();
+            NinjectResolver.Bind<IRoomService>().To<RoomService>();
+
+        }
+
+        public static object Get { get; set; }
+
+        public static IKernel GetDIResolver()
+        {
+            if (NinjectResolver == null)
+                NinjectResolver = new StandardKernel();
+            return NinjectResolver;
+        }
+    }
+}
